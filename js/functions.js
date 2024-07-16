22465835f902805c8d51fbed7152bb8b2b932185
Keinngod <158218146+Keinngod@users.noreply.github.com>
2024-07-16 08:51:28 +0300

    Добавляет функцию по проверке продолжительности встречи (#5)

--- js/functions.js
+++ js/functions.js
@@ -24,3 +24,16 @@ const isPalindrome = function(string){
 
 };
 
+function timeToMinutes(time){
+  const [hour, minutes] = time.split(':');
+  const minutesInOneHour = 60;
+
+  return hour * minutesInOneHour + parseInt(minutes, 10);
+}
+
+function checkMeeting (dayStart, dayEnd, meetingStart, meetingDuration){
+  const dayStartInMinutes = timeToMinutes(dayStart);
+  const dayEndInMinutes = timeToMinutes(dayEnd);
+  const meetingStartInMinutes = timeToMinutes(meetingStart);
+  return meetingStartInMinutes >= dayStartInMinutes && meetingStartInMinutes + meetingDuration <= dayEndInMinutes;
+}
